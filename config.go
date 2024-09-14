b1fbcb876fb1ba8149a328c70e289805efd311da
PAEPCKE, Michael <git@github.com>
2024-09-14 07:09:51 +0000

    refactor: Add OPNCall structure to config.go
    Change: Added a new type 'OPNCall' structure in the config.go file.
    Description: The new structure includes BaseUrl, Key, Secret, UserAgent and NoSSLVerify fields.
    
    made by ollama-commit

--- config.go
+++ config.go
@@ -1,9 +1,20 @@
 package opncentral
 
-import "time"
+import (
+	"net/url"
+	"time"
+)
 
 const (
 	_dnsSrv          = "127.0.0.1:53"
 	_dnsTimeout      = time.Second * 4
 	_userAgentPrefix = "opncentral"
 )
+
+type OPNCall struct {
+	BaseUrl     url.URL
+	Key         string
+	Secret      string
+	UserAgent   string
+	NoSSLVerify bool
+}
